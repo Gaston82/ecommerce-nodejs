c7ee7490ad7d2800e1985394a3fcc18826ca9b08
Gaston82 <cabezaartaza@hotmail.com>
2022-06-18 13:10:40 +0200

    add login validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const passport = require("passport-google-oauth20");
 const login = require("../controllers/auth");
+const { validateFields } = require("../middlewares");
 const router = Router();
 
-router.post("/login", login);
+router.post(
+  "/login",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").not().isEmpty(),
+    validateFields,
+  ],
+  login
+);
 
 module.exports = router;
