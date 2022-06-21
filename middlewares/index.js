b34cd9d1e4cf1ba202900d81835beaa65f8fff8f
Gaston82 <cabezaartaza@hotmail.com>
2022-06-21 05:37:26 +0200

    improve middleware export

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateFields = require("../middlewares/validate-fields");
 const validateJWT = require("../middlewares/validate-jwt");
 const validateRole = require("../middlewares/validate-role");
+const validateFile = require("../middlewares/validate-file");
 
 module.exports = {
   ...validateRole,
   ...validateFields,
   ...validateJWT,
+  ...validateFile,
 };
