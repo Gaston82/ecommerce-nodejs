1d95dfa6d71db26079de96d7567024a5e0ae5131
Gaston82 <cabezaartaza@hotmail.com>
2022-06-21 05:41:21 +0200

    add uploads routes

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,10 +1,41 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { uploadFiles } = require("../controllers/uploads");
+const {
+  uploadFiles,
+  updateImage,
+  showImage,
+  updateImageCloudinary,
+} = require("../controllers/uploads");
+const { validCollections } = require("../helpers");
 
-const { validateFields } = require("../middlewares");
+const { validateFields, validateFile } = require("../middlewares");
 const router = Router();
 
-router.post("/", uploadFiles);
+router.post("/", validateFile, uploadFiles);
+router.put(
+  "/:collection/:id",
+  [
+    validateFile,
+    check("id", "It should be a mongo is").isMongoId(),
+    check("collection").custom((c) =>
+      validCollections(c, ["users", "products"])
+    ),
+    validateFields,
+  ],
+
+  updateImageCloudinary
+);
+
+router.get(
+  "/:collection/:id",
+  [
+    check("id", "It should be a mongo is").isMongoId(),
+    check("collection").custom((c) =>
+      validCollections(c, ["users", "products"])
+    ),
+    validateFields,
+  ],
+  showImage
+);
 
 module.exports = router;
