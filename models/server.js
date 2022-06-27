1f91dc1ae98782b4646cd14827847c08618fb19b
Gaston82 <cabezaartaza@hotmail.com>
2022-06-27 23:27:06 +0200

    add dotenv config

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const cookie = require("cookie-parser");
 const fileUpload = require("express-fileupload");
